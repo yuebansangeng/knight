c96520e7dc056daf989ad6cf4ad067d975383d26
张跃 <zhangyue@zhangyuedeMacBook-Pro.local>
2018-03-09 17:18:31 +0800

    子命令

---
+++ generators/router/index.js
@@ -0,0 +1,8 @@
+
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  method1() {
+    this.log('method 1sdfsdfsdfsdfsdfdsfdsfsdfsdfdsfdsfsdfds just ran');
+  }
+}
