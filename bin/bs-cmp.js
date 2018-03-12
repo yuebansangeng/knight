1f123ad1e042e4c15a329d68a077aa48565555e7
zhangyue <zhangyue@beisen.com>
2018-03-12 19:19:02 +0800

    bug

--- bin/bs-cmp.js
+++ bin/bs-cmp.js
@@ -2,5 +2,5 @@
 
 // 执行指定的 command，不依赖 yo 命令
 require('yeoman-environment').createEnv()
-  .register(require.resolve('./generators/component'), 'bs:cmp')
+  .register(require.resolve('../generators/component'), 'bs:cmp')
   .run('bs:cmp')
