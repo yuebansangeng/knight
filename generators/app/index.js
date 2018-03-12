cdf7ad1ee99327f7b51c15d8c2806623c9546603
zhangyue <zhangyue@beisen.com>
2018-03-12 18:18:57 +0800

    dev cli

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,28 +1,16 @@
 
+var objectAssign = require('object-assign')
 var Generator = require('yeoman-generator')
 
 module.exports = class extends Generator {
 
   prompting () {
-  	return this.prompt([
+    return this.prompt([
       {
-        type: 'input',
-        name: 'cmpName',
-        message: 'This is your component name on flying.'
-      }, {
-        type: 'input',
-        name: 'moduleName',
-        message: 'This is your component moudle name which use on dependencies manage.'
-      }, {
         type: 'list',
-        name: 'type',
-        choices: [ 'Normal Component', 'Shared Component' ],
-        message: 'This is your component type.'
-      }, {
-        type: 'confirm',
-        name: 'hasStorybook',
-        message: 'Are you use storybook ?',
-        default: true
+        name: 'category',
+        choices: [ 'Component', 'Units' ],
+        message: 'Pick your solution.'
       }
     ]).then(promptes => {
       this.promptes = promptes
@@ -30,79 +18,12 @@ module.exports = class extends Generator {
   }
 
   writing () {
-  	this._private_copies([
-      [ 'index.js', 'src/index.js' ],
-      [ 'package.json' ],
-      [ 'webpack.config.js' ],
-      [ '.gitignore' ],
-      [ '.babelrc' ]
-      [ 'README.md' ]
-    ])
-
-    if (this.promptes.type === 'Shared Component') {
-      this._private_copies([[ '.gitlab-ci.yml' ]])
-    }
-
-    if (this.promptes.hasStorybook === true) {
-      this._private_copies([
-        [ '.storybook/index.js' ],
-        [ '.storybook/config.js' ],
-        [ '.storybook/webpack.config.js' ],
-        [ '.storybook/addons.js' ],
-        [ '.babelrc', '.storybook/.babelrc' ]
-      ])
-    }
-  }
-
-  install () {
-    // 修改程序的执行路径到目标文件夹中
-    process.chdir(`${this.contextRoot}`)
-
-  	this.npmInstall(
-      [
-        'babel-cli', 'babel-loader', 'babel-core','babel-preset-env', // babel
-
-        'babel-plugin-transform-object-assign', 'babel-plugin-transform-runtime',
-        'babel-plugin-add-module-exports', 'babel-plugin-transform-decorators-legacy',
-        'babel-plugin-transform-react-display-name', 'babel-plugin-transform-react-jsx', // babel plugins
-
-        'sass-loader', 'less-loader', 'postcss-loader', 'style-loader',
-        'file-loader', 'html-loader', 'markdown-loader', 'url-loader', // webpack loader
-
-        '@storybook/cli', '@storybook/react', 'storybook-readme', // storybook
-
-        'react', 'react-dom', // react
-      ], {
-        'save-dev': true
-      }
-    )
-
-    if (this.promptes.hasStorybook === true) {
-      this.npmInstall(
-        [
-          '@storybook/react'
-        ], {
-          'save-dev': true
-        }
+    if (this.promptes.category === 'Component') {
+      this.composeWith(
+        require.resolve('../component/index.js'),
+        // 子命令中，通过this.contextRoot取不到工作目录
+        objectAssign({ contextRoot: this.contextRoot }, this.promptes)
       )
-    } // if
-  }
-
-  /*
-  * 封装copy（API），减少代码量
-  * 28原则：百分之20%的代码解决80%的功能
-  * 函数名前面添加下滑线，告知Yeoman不自定执行改函数
-  */
-  _private_copies (copyJobs = []) {
-    copyJobs.forEach(([ tplFilePath, destFilePath, tplData = {} ]) => {
-      if (!destFilePath) {
-        destFilePath = tplFilePath
-      }
-      this.fs.copyTpl(
-        this.templatePath(tplFilePath),
-        this.destinationPath(`${this.contextRoot}/${destFilePath}`),
-        this.promptes
-      )
-    })
+    }
   }
 }
---
+++ generators/component/index.js
@@ -0,0 +1,115 @@
+
+var Generator = require('yeoman-generator')
+
+module.exports = class extends Generator {
+
+  configuring () {
+    // 如果参数重配置 contextRoot，则使用传入的变量
+    if (this.options.contextRoot) {
+      this.contextRoot = this.options.contextRoot  
+    }
+  }
+
+  prompting () {
+  	return this.prompt([
+      {
+        type: 'input',
+        name: 'cmpName',
+        message: 'This is your component name on flying.'
+      }, {
+        type: 'input',
+        name: 'moduleName',
+        message: 'This is your component moudle name which use on dependencies manage.'
+      }, {
+        type: 'list',
+        name: 'type',
+        choices: [ 'Normal Component', 'Shared Component' ],
+        message: 'This is your component type.'
+      }, {
+        type: 'confirm',
+        name: 'hasStorybook',
+        message: 'Are you use storybook ?',
+        default: true
+      }
+    ]).then(promptes => {
+      this.promptes = promptes
+    })
+  }
+
+  writing () {
+  	this._private_copies([
+      [ 'index.js', 'src/index.js' ],
+      [ 'package.json' ],
+      [ 'webpack.config.js' ],
+      [ '.gitignore' ],
+      [ '.babelrc' ],
+      [ 'README.md' ]
+    ])
+
+    if (this.promptes.type === 'Shared Component') {
+      this._private_copies([[ '.gitlab-ci.yml' ]])
+    }
+
+    if (this.promptes.hasStorybook === true) {
+      this._private_copies([
+        [ '.storybook/index.js' ],
+        [ '.storybook/config.js' ],
+        [ '.storybook/webpack.config.js' ],
+        [ '.storybook/addons.js' ],
+        [ '.babelrc', '.storybook/.babelrc' ]
+      ])
+    }
+  }
+
+  install () {
+    // 修改程序的执行路径到目标文件夹中
+    process.chdir(`${this.contextRoot}`)
+
+  	this.npmInstall(
+      [
+        'babel-cli', 'babel-loader', 'babel-core','babel-preset-env', // babel
+
+        'babel-plugin-transform-object-assign', 'babel-plugin-transform-runtime',
+        'babel-plugin-add-module-exports', 'babel-plugin-transform-decorators-legacy',
+        'babel-plugin-transform-react-display-name', 'babel-plugin-transform-react-jsx', // babel plugins
+
+        'sass-loader', 'less-loader', 'postcss-loader', 'style-loader',
+        'file-loader', 'html-loader', 'markdown-loader', 'url-loader', // webpack loader
+
+        '@storybook/cli', '@storybook/react', 'storybook-readme', // storybook
+
+        'react', 'react-dom', // react
+      ], {
+        'save-dev': true
+      }
+    )
+
+    if (this.promptes.hasStorybook === true) {
+      this.npmInstall(
+        [
+          '@storybook/react'
+        ], {
+          'save-dev': true
+        }
+      )
+    } // if
+  }
+
+  /*
+  * 封装copy（API），减少代码量
+  * 28原则：百分之20%的代码解决80%的功能
+  * 函数名前面添加下滑线，告知Yeoman不自定执行改函数
+  */
+  _private_copies (copyJobs = []) {
+    copyJobs.forEach(([ tplFilePath, destFilePath, tplData = {} ]) => {
+      if (!destFilePath) {
+        destFilePath = tplFilePath
+      }
+      this.fs.copyTpl(
+        this.templatePath(tplFilePath),
+        this.destinationPath(`${this.contextRoot}/${destFilePath}`),
+        this.promptes
+      )
+    })
+  }
+}
