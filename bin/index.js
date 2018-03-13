a6e4b704fdcf77d7028d42bf5a5ced42c7d16bd2
zhangyue <zhangyue@beisen.com>
2018-03-13 12:18:28 +0800

    new bin

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
 
+// todo
+// * 使用现有库，实现 --help，等标准命令附加功能
+
+// 获取nodejs命令的参数
+// 前两个参数默认传递，不需要考虑
+var [
+  nodePath,
+  nodeScriptPath,
+  secondCmd
+] = process.argv
+
 // 执行指定的 command，不依赖 yo 命令
-require('yeoman-environment').createEnv()
+var env = require('yeoman-environment').createEnv()
   .register(require.resolve('../generators/scaffold'), 'scaffold')
-  .run('scaffold Component')
+
+// bscpm create:cmp
+if (secondCmd === 'create:cmp') {
+  env.run('scaffold Component')
+}
