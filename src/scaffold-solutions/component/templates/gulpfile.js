1697a413d24780d21c472ce1fb096fd0f0c7d66a
zhangyue <zygeilit@icloud.com>
2018-04-29 21:44:22 +0800

    new

--- src/scaffold-solutions/component/templates/gulpfile.js
+++ src/scaffold-solutions/component/templates/gulpfile.js
@@ -6,21 +6,29 @@ const cssbeautify = require('gulp-cssbeautify')
 const replace = require('gulp-replace')
 const del = require('del')
 
-gulp.task('default', function() {
+gulp.task('default', [ 'scripts', 'style' ], function() {
+  del.sync([ 'lib/**' ])
+})
 
-  del.sync(['lib/**'])
+// 监控
+gulp.task('watch', function () {
+  gulp.watch([ 'src/**/*.js', './src/**/*.scss', './src/**/*.css' ], [ 'style', 'scripts' ])
+})
 
-  // 解析scss文件到css
-  gulp.src([ './src/**/*.scss', './src/**/*.css' ])
-    .pipe(sass.sync().on('error', sass.logError))
-    // 缩进2个空格
-    .pipe(cssbeautify({ indent: '  ' }))
-    .pipe(gulp.dest('lib'))
-
-  // es6代码转义es5
+// es6代码转义es5
+gulp.task('style', function () {
   gulp.src('src/**/*.js')
     .pipe(babel())
     // 替换 js 文件中引用的 .scss 后缀为 .css
     .pipe(replace(/require\((['"])(.+?)(\.scss)['"]\)/g, 'require($1$2.css$1)'))
+    .pipe(gulp.dest('lib'))
+})
+
+// 解析scss文件到css
+gulp.task('scripts', function () {
+  gulp.src([ './src/**/*.scss', './src/**/*.css' ])
+    .pipe(sass.sync().on('error', sass.logError))
+    // 缩进2个空格
+    .pipe(cssbeautify({ indent: '  ' }))
     .pipe(gulp.dest('lib'))
 })
