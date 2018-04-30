bf7a616ba73fc2ae90f9942322adfff5c46d39e7
zhangyue <zygeilit@icloud.com>
2018-04-30 15:02:47 +0800

    new

--- src/scaffold-solutions/component/index.js
+++ src/scaffold-solutions/component/index.js
@@ -24,7 +24,6 @@ module.exports = class extends Generator {
   	this._private_copies([
       [ '.babelrc' ],
       [ '.publish', '.build/.publish' ],
-      [ '.sandbox.js', '.build/.sandbox.js' ],
       [ 'gitignore', '.gitignore' ], // npm publish，会忽略 .gitignore 文件
       [ 'index.js', 'src/index.js' ],
       [ 'BaseComponent.js', 'src/BaseComponent.js' ],
--- src/scaffold-solutions/component/templates/.sandbox.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import { render } from "react-dom"
-import Component from "../src"
-
-render(<Component />, document.getElementById("root"));
