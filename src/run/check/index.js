7d12cdd17bbaeeb2cb4eff2f79acd9e5fbe5d8d5
zhangyue <zygeilit@icloud.com>
2018-06-05 17:58:30 +0800

    new record

--- src/run/check/index.js
+++ src/run/check/index.js
@@ -7,11 +7,18 @@ module.exports = class extends Generator {
 
   // 统一添加前缀组件模块前缀
   async writing () {
-    let { name, id } = require(`${this.contextRoot}/.bscpmrc.json`)
+    let { name, team } = require(`${this.contextRoot}/.bscpmrc.json`)
     let { 'name': module } = require(`${this.contextRoot}/package.json`)
 
+    if (!name) {
+      throw new Error('请在 .bscpmrc.json 文件中，配置 name 字段')
+    }
+    if (!team) {
+      throw new Error('请在 .bscpmrc.json 文件中，配置 team 字段（team 字段将会用来组件唯一性验证，以及搜索功能）')
+    }
+
     let { code, message, data } = await new Promise((resolve, reject) => {
-      request(`http://cmp.beisen.io/users/check-cmp?name=${name||''}&id=${id||''}&module=${module||''}`, (err, res, body) => {
+      request(`http://cmp.beisen.io/users/check-cmp?name=${name||''}&team=${team||''}&module=${module||''}`, (err, res, body) => {
         if (err) {
           console.log(err)
           reject(err)
