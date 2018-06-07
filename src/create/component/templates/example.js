d5b09442317a61a00877f39aa8a9529736a7e2ea
zhangyue <zygeilit@icloud.com>
2018-06-07 14:52:32 +0800

    n

--- src/create/component/templates/example.js
+++ src/create/component/templates/example.js
@@ -1,7 +1,6 @@
 
 import React, { Component } from 'react'
-// @ 会默认获取当前组件
-import DefaultComponent from '@component'
+import DefaultComponent from '../../src'
 
 export default class extends Component {
   render () {
