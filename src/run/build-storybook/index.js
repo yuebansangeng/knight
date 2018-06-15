7f2b4951a842a8a618aa14f7c3ddaa4845f14fd8
zhangyue <zygeilit@icloud.com>
2018-06-15 10:54:00 +0800

    new build

--- src/run/build-storybook/index.js
+++ src/run/build-storybook/index.js
@@ -2,6 +2,7 @@
 const Generator = require('yeoman-generator')
 const Promise = require('bluebird')
 const { spawn } = require('child_process')
+const path = require('path')
 
 module.exports = class extends Generator {
 
@@ -12,10 +13,13 @@ module.exports = class extends Generator {
 
     console.log(`${name}/${version} 编译中...`)
 
+    // 定位到 bscpm 根级目录
+    const rootPath = path.join(__dirname, '..', '..', '..')
+
     await new Promise((resolve, reject) => {
       let resmsg = []
 
-      let build_cp = spawn('node', [ 'node_modules/@storybook/react/bin/build.js', '-c', `${this.contextRoot}/node_modules/@beisen/storybook-lib/lib`, '-o', `./${name}/${version}` ])
+      let build_cp = spawn('node', [ `${rootPath}/node_modules/@storybook/react/bin/build.js`, '-c', `${this.contextRoot}/node_modules/@beisen/storybook-lib/lib`, '-o', `./${name}/${version}` ])
       build_cp.stdout.on('data', data => resmsg.push(`${data}`))
       build_cp.stderr.on('data', data => resmsg.push(`${data}`))
 
