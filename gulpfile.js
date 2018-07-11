38477a1c7d67959d1b0ce4fe2f2de4790752763e
zhangyue <zygeilit@icloud.com>
2018-07-11 22:03:29 +0800

    new

--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,25 @@ const gulp = require('gulp')
 const babel = require('gulp-babel')
 const del = require('del')
 
-gulp.task('default', [ 'clear', 'scripts', 'other', 'scripts-ejs', 'bin' ])
-
-// 清理lib文件夹内的文件
-gulp.task('clear', function () {
+gulp.task('default', function () {
   del.sync('lib')
-  del.sync('bin')
-})
 
-// es6代码转义es5
-gulp.task('scripts', function () {
-  return gulp.src('src/**/*.js')
+  gulp.src([ 'src/run/**/*.js' ])
     .pipe(babel())
-    .pipe(gulp.dest('lib'))
-})
+    .pipe(gulp.dest('lib/run'))
 
-gulp.task('scripts-ejs', function () {
-  return gulp.src('src/**/*.ejs')
-    .pipe(gulp.dest('lib'))
-})
+  gulp.src([ 'src/create/index.js', ])
+    .pipe(babel())
+    .pipe(gulp.dest('lib/create'))
 
-// 复制图片文件到lib文件夹
-gulp.task('other', function () {
-  // (png|jpe?g|gif|svg)
-  return gulp.src([ 'src/**/*.html', 'src/**/*.json', 'src/**/*.ejs' ])
-    .pipe(gulp.dest('lib'))
-})
+   gulp.src([ 'src/create/component/index.js' ])
+    .pipe(babel())
+    .pipe(gulp.dest('lib/create/component'))
 
-// es6代码转义es5
-gulp.task('bin', function () {
-  return gulp.src('src-bin/**/*.js')
-    .pipe(babel(babelrcJson))
-    .pipe(gulp.dest('bin'))
+  gulp.src([ 'src/*.js' ])
+    .pipe(babel())
+    .pipe(gulp.dest('lib/'))
+
+  gulp.src([ 'src/create/component/templates/*' ])
+    .pipe(gulp.dest('lib/create/component/templates/'))
 })
