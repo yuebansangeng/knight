b98e57b8219403f544e0ac951d823971ccfd8018
zhangyue <zygeilit@icloud.com>
2018-07-20 11:09:30 +0800

    remove useless code

--- src/run/publish/get-file-content.js
+++ src/run/publish/get-file-content.js
@@ -2,20 +2,11 @@
 import  fs from 'fs'
 
 export default {
-
   getContent (filepath, utf8 = true) {
     if (!fs.existsSync(filepath)) {
       return null
     }
     return utf8 ? fs.readFileSync(filepath, 'utf8') :
       fs.readFileSync(filepath)
-  },
-
-  getJson (filepath, utf8 = true) {
-    if (!fs.existsSync(filepath)) {
-      return null
-    }
-    let content = fs.readFileSync(filepath, 'utf8')
-    return JSON.parse(content)
   }
 }
